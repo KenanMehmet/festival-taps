82337f1dd4afdf57b21b0f27f55c4966f42d4c0d
KenanMehmet <kenmehmet1@gmail.com>
2023-03-27 17:22:56 +0100

    started work on creating error validation, dinner break started

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from test_data import testing_data
+from test_data import testing_data, testing_error_data
 
 """
 Received email on Monday 27th 2:15PM
@@ -11,11 +11,7 @@ Function should return the total number of seconds that it takes for all people 
 Assume that once a tap is free it next peson uses that tapp immiedetly and cannot move to a different tap once they started filling
 Each tap flows at a rate of 100ml per second (e.g 1 litre bottle takes 10 seconds)
 
-basic flowchart.
-
-if free tap:
-    go to tap
-
+Finished inital function 16:36PM
 """
 
 tap_fill_rate = 100
@@ -36,6 +32,15 @@ def FillUpBottle(water_bottles, taps):
         occupied_taps[occupied_taps.index(0)] += bottle #Find an unoccupied tap and add how long it will take to fill up that bottle to 
     return max([taps[i] + occupied_taps[i] for i in range(0, len(taps))])
 
+
+"""
+Conceived a more efficent way of coming up with the correct answer based on looking at the test data
+
+Instead of creating two arrays that would add to each other, one array is only needed and we just need to count how much time each array was being used for.
+Then we find the max and that is how long it takes for each bottle to be filled
+
+Finished inital function 17:07pm
+"""
 def FillUpBottleVersionTwo(water_bottles, taps):
     taps = [0] * taps
     water_bottles = [bottle / 100 for bottle in water_bottles]
@@ -43,7 +48,24 @@ def FillUpBottleVersionTwo(water_bottles, taps):
         taps[taps.index(min(taps))] += bottle
     return max(taps)
 
+
+"""
+Step two:
+Adding in data validation, we want to return an error if the data is invalid and explain why.
+"""
+def ValidateData(water_bottles, taps):
+    try:
+        FillUpBottleVersionTwo(water_bottles, taps)
+    except Exception as e:
+        print(e)
+    
+
+
 if __name__ == "__main__":
+    """
     for data in testing_data:
         print(FillUpBottle(data[0], data[1]))
         print(FillUpBottleVersionTwo(data[0], data[1]))
+    """
+    for data in testing_error_data:
+        print(ValidateData(data[0], data[1]))
