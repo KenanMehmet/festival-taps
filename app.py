f922ca5e02f71496e6af573826b4a168351cded1
KenanMehmet <kenmehmet1@gmail.com>
2023-03-27 15:16:31 +0100

    added test data file, timestamps for work and inital function

---
+++ app.py
@@ -0,0 +1,17 @@
+from test_data import test_data
+
+"""
+Received email on Monday 27th 2:15PM
+Started at 14:45PM
+
+Step 1: Create a function that takes two inputs: 
+    Array of integers
+    Integer to represent taps
+Function should return the total number of seconds that it takes for all people to have filled their water bottles.
+Assume that once a tap is free it next peson uses that tapp immiedetly and cannot move to a different tap once they started filling
+Each tap flows at a rate of 100ml per second (e.g 1 litre bottle takes 10 seconds)
+"""
+
+
+def FillTap(water_bottles, taps):
+    return True
